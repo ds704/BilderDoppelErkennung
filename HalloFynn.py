1ed37906ea39b5ffedcf12bbcd5916214bade911
ds704 <daniel.s704@outlook.com>
2022-11-23 22:33:53 +0100

    Für den lieben Fynn hier ein ERfolgserlebnis

--- HalloFynn.py
+++ HalloFynn.py
@@ -1,11 +1,16 @@
 #https://colab.research.google.com/drive/17q-vNL4HQoKg7907A4SogxPYImqJlMD-
-from PIL import Image, ImageChope
+from PIL import Image, ImageChops
 
-def equal(img1,img2)
-    return ImageChope.diffrence(img1,img2).getbbox() is None
+def equal(img1,img2):
+    return ImageChops.difference(img1,img2).getbbox() is None
 
-im1= Image.open("DateiPfad")#durch echten Datpfad erstezen
-im2 = Image.open("DateiPfad")
+im1 = Image.open("/home/ds704/Downloads/Putin1.jpeg")#durch echten Datpfad erstezen
+im2 = Image.open("/home/ds704/Downloads/Selensky.jpeg")
+im3 = Image.open("/home/ds704/Downloads/Putin2.jpeg")
 
 result = equal(im1,im2)
+print("Sind Putin und Selensky gleich?")
+print(str(result))
+print("Sind Putin und Putin gleich?")
+result = equal(im1,im3)
 print(str(result))
