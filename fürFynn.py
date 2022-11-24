a5b7ca5606d74bac1f7e528c298d123e2e078d19
ds704 <42025411+ds704@users.noreply.github.com>
2022-11-24 12:27:31 +0100

    Finale Version

--- fürFynn.py
+++ fürFynn.py
@@ -1,8 +1,14 @@
 import os
+import hashlib
+
+def file_hash(filepath):
+    with open(filepath, 'rb') as f:
+        return md5(f.read()).hexdigest()
+
 os.getcwd()
-os.chdir(r'C:\Users\\Privat\Projekte\Python\BilderDuplicatfinder')
+os.chdir(r'/home/ds704/Downloads')
 os.getcwd()
-file_list = os.listdir()
+file_list = os.listdir('.')
 print(len(file_list))
 duplicates = []
 hash_keys = dict()
@@ -10,8 +16,15 @@ for index, filename in  enumerate(os.listdir('.')):  #listdir('.') = current dir
     if os.path.isfile(filename):
         with open(filename, 'rb') as f:
             print(filename)
-            #filehash = hashlib.md5(f.read()).hexdigest()
-        #if filehash not in hash_keys:
-          #  hash_keys[filehash] = index
-        #else:
-           # duplicates.append((index,hash_keys[filehash]))
+            filehash = hashlib.md5(f.read()).hexdigest()
+        if filehash not in hash_keys:
+            hash_keys[filehash] = index
+        else:
+            duplicates.append((index,hash_keys[filehash]))
+
+print(duplicates)
+
+
+for index in duplicates:
+    os.remove(file_list[index[0]])
+
